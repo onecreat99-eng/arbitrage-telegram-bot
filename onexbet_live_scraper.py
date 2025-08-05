a95fbf6341219682ae43f89d179523cefe20226d
onecreat99-eng <onecreat99@gmail.com>
2025-08-06 03:11:45 +0530

    Create onexbet_live_scraper.py

---
+++ onexbet_live_scraper.py
@@ -0,0 +1,15 @@
+def get_1xbet_live_odds():
+    return [
+        {
+            "team1": "Team A",
+            "team2": "Team B",
+            "odds1": 2.5,
+            "odds2": 2.6
+        },
+        {
+            "team1": "Team C",
+            "team2": "Team D",
+            "odds1": 1.8,
+            "odds2": 2.2
+        }
+    ]

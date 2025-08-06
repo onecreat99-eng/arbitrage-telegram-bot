fe4b822a4e2084f2fe7080016483b263ed453bf1
onecreat99-eng <onecreat99@gmail.com>
2025-08-06 13:23:38 +0530

    Delete stake_scraper.py

--- stake_scraper.py
+++
@@ -1,7 +0,0 @@
-def get_stake_live_odds():
-    # Your actual Stake live odds logic here
-    return []
-
-def get_stake_prematch_odds():
-    # Your actual Stake prematch odds logic here
-    return []
